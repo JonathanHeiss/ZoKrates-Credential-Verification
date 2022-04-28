5562620243a5a5143ee3bc6c18bada625d1e07d5
robmuth <51814837+robmuth@users.noreply.github.com>
2022-04-28 19:11:17 +0200

    equality pattern

---
+++ patterns/equality/eval.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+export PATH=$PATH:/Users/$USER/.zokrates/bin
+
+# Attestation
+
+cd $DIR/attestation
+python3.9 -m virtualenv venv
+source venv/bin/activate
+
+python3.9 zokgen.py > $DIR/artifacts/witness-parameters.txt
+
+# Proving
+
+cd $DIR/proving
+zokrates compile -i verify_equality.zok -o $DIR/artifacts/out
+
+START=`date +%s`
+cat ../artifacts/witness-parameters.txt | xargs zokrates compute-witness -i $DIR/artifacts/out -o $DIR/artifacts/witness -a 
+END=`date +%s`
+witnessDur=$(echo "$END - $START" | bc)
+
+mv abi.json $DIR/artifacts/
+
+# Zokrates setup
+START=`date +%s`
+zokrates setup -i $DIR/artifacts/out -p $DIR/artifacts/proving.key -v $DIR/artifacts/verification.key
+END=`date +%s`
+compileDur=$(echo "$END - $START" | bc)
+
+# Verification
+zokrates export-verifier -i $DIR/artifacts/verification.key -o $DIR/verification/contracts/Verifier.sol
+
+START=`date +%s`
+zokrates generate-proof -i $DIR/artifacts/out -j $DIR/artifacts/proof.json -p $DIR/artifacts/proving.key -w $DIR/artifacts/witness
+END=`date +%s`
+proofDur=$(echo "$END - $START" | bc)
+
+# Statistics
+echo "Witness: $witnessDur sec."
+echo "Setup: $setupDur sec."
+echo "Proof: $proofDur sec."
